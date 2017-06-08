c9abbc0ddcfdf0b0193dfd1e44237f8386ce0706
myt00seven <myt007@gmail.com>
2017-06-08 03:43:49 -0500

    fix var

--- tutorials/03-advanced/variational_auto_encoder/main_ori.py
+++ tutorials/03-advanced/variational_auto_encoder/main_ori.py
@@ -65,7 +65,7 @@ data_iter = iter(data_loader)
 # fixed inputs for debugging
 fixed_z = to_var(torch.randn(100, 20))
 fixed_x, _ = next(data_iter)
-torchvision.utils.save_image(fixed_x, './data/real_images.png')
+torchvision.utils.save_image(fixed_x.data.cpu(), './data/real_images.png')
 fixed_x = to_var(fixed_x.view(fixed_x.size(0), -1))
 
 for epoch in range(50):
@@ -94,5 +94,5 @@ for epoch in range(50):
     # Save the reconstructed images
     reconst_images, _, _ = vae(fixed_x)
     reconst_images = reconst_images.view(reconst_images.size(0), 1, 28, 28)
-    torchvision.utils.save_image(reconst_images, 
+    torchvision.utils.save_image(reconst_images.data.cpu(), 
         './data/reconst_images_%d.png' %(epoch+1))
